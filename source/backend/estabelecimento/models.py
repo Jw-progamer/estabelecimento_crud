16ffd21bf7592cae527fb7e3518c45b25a6489b5
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2019-10-14 22:45:34 -0300

    Normalizar Git Flow

---
+++ source/backend/estabelecimento/models.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+
+
+class Estabelecimento(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    cnpj = models.CharField(max_length=11, unique=True)
+    nome = models.CharField(max_length=15)
+    endereco = models.TextField()
+    raio_distancia = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Estabelecimento"
+        verbose_name_plural = "Estabelecimentos"
