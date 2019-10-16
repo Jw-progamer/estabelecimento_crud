d0e39c9f6077882a2488b3ad2e58b2b80ce06cce
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2019-10-16 13:07:53 -0300

    Um botao para deslogar caso necessario

--- source/frontend/src/utils.js
+++ source/frontend/src/utils.js
@@ -1,11 +1,11 @@
 function getHeaders() {
-    const token = localStorage.getItem('token');
-    return {
-        'Content-Type': 'application/json',
-        ...token && {
-            'Authorization': `Bearer ${token}`
-        }
-    };
+  const token = localStorage.getItem("token");
+  return {
+    "Content-Type": "application/json",
+    ...(token && {
+      Authorization: `Bearer ${token}`
+    })
+  };
 }
 
 export default getHeaders;
--- source/frontend/src/views/EstabelecimentoPage.vue
+++ source/frontend/src/views/EstabelecimentoPage.vue
@@ -1,34 +1,37 @@
 <template>
   <v-content>
     <div v-for="estabelecimento in estabelecimentos" :key="estabelecimento.id">
-      <h1>{{estabelecimento.nome}}</h1>
-      <h2>{{estabelecimento.cnpj}}</h2>
-      <h6>{{estabelecimento.raio_distancia}}</h6>
+      <h1>{{ estabelecimento.nome }}</h1>
+      <h2>{{ estabelecimento.cnpj }}</h2>
+      <h6>{{ estabelecimento.raio_distancia }}</h6>
     </div>
   </v-content>
 </template>
 
 <script>
-import axios from "axios"
-import getHeaders from "../utils"
+import axios from "axios";
+import getHeaders from "../utils";
 
 export default {
-    name: "estabelecimentos",
-    data(){
-      return{
-        estabelecimentos:[]
-      }
-    },
-    created(){
-      axios.get("http://localhost:8000/api/estabelecimentos",{
-        'headers': getHeaders()
-      }).then( response => {
-        this.estabelecimentos = response.data
-      }).catch( response => {
-        console.log(response)
+  name: "estabelecimentos",
+  data() {
+    return {
+      estabelecimentos: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/estabelecimentos", {
+        headers: getHeaders()
       })
-    }
-}
+      .then(response => {
+        this.estabelecimentos = response.data;
+      })
+      .catch(response => {
+        console.log(response);
+      });
+  }
+};
 </script>
 
 <style></style>
