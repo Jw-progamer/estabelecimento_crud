edb698136ba18279a99bea248f4a0843db053f15
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2019-10-16 21:34:17 -0300

    Tela de visualizçao melçhor estilizada e com links feito. Proximo passo, formulario

---
+++ source/frontend/src/components/estabelecimentos/Estabelecimento.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>
+        <router-link
+          :to="{path: '/edit/'+estabelecimento['id'], params:estabelecimento}"
+        >{{estabelecimento.nome}}</router-link>
+      </v-list-item-title>
+      <v-list-item-subtitle>{{estabelecimento.cnpj}}</v-list-item-subtitle>
+      <v-list-item-subtitle>{{estabelecimento.endereco}}</v-list-item-subtitle>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "Estabelecimento",
+  props: {
+    estabelecimento: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ source/frontend/src/components/estabelecimentos/ListaEstabelecimento.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-content>
+    <v-list three-line>
+      <template v-for="estabelecimento in estabelecimentos">
+        <Estabelecimento :estabelecimento="estabelecimento" :key="estabelecimento['id']" />
+      </template>
+    </v-list>
+  </v-content>
+</template>
+
+<script>
+import axios from "axios";
+import getHeaders from "../../utils";
+
+import Estabelecimento from "./Estabelecimento";
+
+export default {
+  name: "ListaEstabelecimentos",
+  components: {
+    Estabelecimento
+  },
+  data() {
+    return {
+      estabelecimentos: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/estabelecimentos", {
+        headers: getHeaders()
+      })
+      .then(response => {
+        this.estabelecimentos = response.data;
+      })
+      .catch(response => {
+        console.log(response);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
